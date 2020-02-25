8019d1ee08cdc65aa26b2fb44a796072aea374bd
zhouhuiping <zhouhuiping@souche.com>
2020-02-25 17:42:11 +0800

    fix: 修复解析文件时bug

--- src/parser.js
+++ src/parser.js
@@ -22,7 +22,7 @@ class Parser {
         liner.open(file);
         var lineNumber = 0;
         var lineContent = '';
-        var currentTable = this.tables[0];
+        var currentTable;
         while (!liner.EOF) {
             /// 文件行号从1开始
             lineNumber += 1;
@@ -31,50 +31,52 @@ class Parser {
             // console.log(lineNumber + '  |  '  + lineContent);
             /// 如果是*** xxx ***开头则创建一个table
             if (String(lineContent).indexOf('***') == 0) {
-                var settingReg = RegExp(/Settings/)
-                var keyWordsReg = RegExp(/Keywords/)
-                var testCaseReg = RegExp(/Test Case/)
-                var variablesReg = RegExp(/Variable/)
-                var lastTabel = NaN
+                var settingReg = RegExp(/Settings/);
+                var keyWordsReg = RegExp(/Keywords/);
+                var testCaseReg = RegExp(/Test Case/);
+                var variablesReg = RegExp(/Variable/);
+                var lastTabel = NaN;
                 if (this.tables.length > 0) {
-                    lastTabel = this.tables[this.tables.length - 1]
-                } 
+                    lastTabel = this.tables[this.tables.length - 1];
+                }
                 /// 构建不同类型的table
                 if (settingReg.test(String(lineContent))) {
                     let Stable = new table(lastTabel, `Settings`, lineContent,lineNumber);
                     /// 更新currentTable
-                    this.refreshTable(currentTable)
+                    this.refreshTable(currentTable);
                     this.tables.push(Stable);
                     currentTable = Stable;
                 } else if (keyWordsReg.test(String(lineContent))) {
                     let Ktable = new table(lastTabel, 'Keywords', lineContent, lineNumber);
-                    this.refreshTable(currentTable)
+                    this.refreshTable(currentTable);
                     this.tables.push(Ktable);
                     currentTable = Ktable;
                 } else if (testCaseReg.test(String(lineContent))) {
                     let Ttable = new table(lastTabel, 'Test Case', lineContent, lineNumber);
-                    this.refreshTable(currentTable)
+                    this.refreshTable(currentTable);
                     this.tables.push(Ttable);
                     currentTable = Ttable;
                 } else if (variablesReg.test(String(lineContent))) {
                     let Vtable = new table(lastTabel, 'Variable', lineContent, lineNumber);
-                    this.refreshTable(currentTable)
+                    this.refreshTable(currentTable);
                     this.tables.push(Vtable);
                     currentTable = Vtable;
-                } 
+                }
             } else {
                 let cells = this.parserCells(lineContent);
-                let row = new Row(lineNumber,cells)
-                currentTable.rows.push(row)
+                let row = new Row(lineNumber,cells);
+                if (currentTable != undefined) {
+                    currentTable.rows.push(row);
+                }
             }
         }
     }
 
     /// 更新currentTable
-    refreshTable(currentTable) {
-        if (this.tables.length > 0) {
+    refreshTable(table) {
+        if (this.tables.length > 0 && table != undefined) {
             this.tables.pop();
-            this.tables.push(currentTable);
+            this.tables.push(table);
         }
     }
 
@@ -107,7 +109,7 @@ class Parser {
                     }
                     let cell =  new Cell(currentWord.join(''),i - currentWord.length-1);
                     cells.push(cell);
-                    currentWord = []
+                    currentWord = [];
                 }
             } 
             /// 加上最后一个word
@@ -115,11 +117,11 @@ class Parser {
                 if (currentWord[currentWord.length-1] == ' ') {
                     currentWord.pop();
                 }
-                let cell = new Cell(currentWord.join(''),contentStr.length - currentWord.length)
+                let cell = new Cell(currentWord.join(''),contentStr.length - currentWord.length);
                 cells.push(cell);
             }
         }
-        return cells
+        return cells;
     }
 }
 
